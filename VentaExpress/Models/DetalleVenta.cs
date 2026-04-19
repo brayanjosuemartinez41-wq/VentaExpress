b5ff143de05a7367e27cc18ef2844d84a0c8d063
BrayanMartinez <brayanjosuemartinez41@gmail.com>
2026-04-19 13:26:39 -0600

    Agregamos entidades Clientes, Prodectos, Venta y DetalleVenta

---
+++ VentaExpress/Models/DetalleVenta.cs
@@ -0,0 +1,8 @@
+﻿public class DetalleVenta
+{
+    public int Id { get; set; }
+    public int VentaId { get; set; }
+    public int ProductoId { get; set; }
+    public int Cantidad { get; set; }
+    public decimal Precio { get; set; }
+}
---
+++ VentaExpress/Models/Venta.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Venta
+{
+    public int Id { get; set; }
+    public DateTime Fecha { get; set; }
+    public int ClienteId { get; set; }
+    public decimal Total { get; set; }
+}
